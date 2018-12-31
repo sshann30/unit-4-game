590ea699be0213a1d8e55b0b3c8eba0b5d05b071
Steve Shannon <you@example.com>
2018-12-30 20:40:20 -0500

    work with j on sunday

--- assets/javascript.js
+++ assets/javascript.js
@@ -1,71 +1,69 @@
-	var wins = 0;
-	var losses = 0;
-	var currentScore = 0;
-  
-	var getRandomNumber = function (min, max) {
-	  return Math.floor(Math.random() * (max - min + 1)) + min;
-	};
+// variables
+var wins = 0;
+var losses = 0;
+var currentScore = 0;
+var barca = Math.floor(Math.random() * 12 + 1);
+var bayern = Math.floor(Math.random() * 12 + 1);
+var manu = Math.floor(Math.random() * 12 + 1);
+var psg = Math.floor(Math.random() * 12 + 1);
+var randomNumber = Math.floor(Math.random() * 102 + 19);
+$("#answer").html(randomNumber);
 
-	var randomNumber = getRandomNumber(10, 20);
-	$("#randomNumber").html(randomNumber);
-  
-	var barca = getRandomNumber(1, 12);
-	var bayern = getRandomNumber(1, 12);
-	var manu = getRandomNumber(1, 12);
-	var psg = getRandomNumber(1, 12);
-  
-	var newValues = function() {
-	  barca = getRandomNumber(1, 12);
-	  bayern = getRandomNumber(1, 12);
-	  manu = getRandomNumber(1, 12);
-	  psg = getRandomNumber(1, 12);
-	};
-  
+//click on a button to bring the score up
+$("#barca").click(function () {
+	currentScore += barca
 	$("#currentScore").html(currentScore);
-  
-	$("#barca").on("click", function() {
-	  currentScore += barca;
-	  $("#currentScore").html(currentScore);
-	  updateScore();
-	});
-  
-	$("#bayern").on("click", function() {
-	  currentScore += bayern;
-	  $("#currentScore").html(currentScore);
-	  updateScore();
-	});
-  
-	$("#manu").on("click", function() {
-	  currentScore += manu;
-	  $("#currentScore").html(currentScore);
-	  updateScore();
-	});
-  
-	$("#psg").on("click", function() {
-	  currentScore += psg;
-	  $("#currentScore").html(currentScore);
-	  updateScore();
-	});
-  
-	var updateScore = function() {
-	  if (randomNumber === currentScore) {
-		wins++;
-		$("#wins").html(wins);
-		$("#randomNumber").empty();
-		randomNumber = getRandomNumber(19,120);
-		$("#randomNumber").html(randomNumber);
-		currentScore = 0;
-		$("#currentScore").html(currentScore)
-		newValues();
-	  }
-	  if (randomNumber < currentScore) {
-		losses++;
-		$("#losses").html(losses);
-		$("#randomNumber").empty();
-		randomNumber = getRandomNumber(19,120);
-		$("#randomNumber").html(randomNumber);
-		currentScore = 0;
-		$("#currentScore").html(currentScore)
-		newValues();
-	  }
-	};
+	checkWinOrLoss()
+});
+$("#bayern").click(function () {
+	currentScore += bayern
+	$("#currentScore").html(currentScore);
+	checkWinOrLoss()
+});
+$("#manu").click(function () {
+	currentScore += manu
+	$("#currentScore").html(currentScore);
+	checkWinOrLoss()
+});
+$("#psg").click(function () {
+	currentScore += psg
+	$("#currentScore").html(currentScore);
+	checkWinOrLoss()
+});
+
+function checkWinOrLoss() {
+	if (currentScore === randomNumber){
+		win()
+	}
+	else if (currentScore > randomNumber){
+		loss()
+	}
+}
+
+
+//scoring
+function win() {
+	alert("You know your teams");
+	wins++;
+	$('#wins').text(wins);
+	reset();
+	console.log
+
+};
+function loss() {
+	alert("You aren't a champion this time");
+	losses++;
+	$('#losses').text(losses);
+	reset();
+};
+
+function reset() {
+	randomNumber = Math.floor(Math.random() * 102 + 19);
+	currentScore = 0;
+	barca = Math.floor(Math.random() * 12 + 1);
+	bayern = Math.floor(Math.random() * 12 + 1);
+	manu = Math.floor(Math.random() * 12 + 1);
+	psg = Math.floor(Math.random() * 12 + 1);
+	$("#currentScore").html(currentScore);
+	$("#answer").html(randomNumber);
+}
